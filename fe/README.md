277c44b70878d00a4929e29b36f7109e667341c0
Jeane Erwansyah <blcktmyie123@gmail.com>
2023-03-20 00:13:24 +0700

    feat(fe): add timer

--- fe/README.md
+++ fe/README.md
@@ -2,4 +2,4 @@
 
 1. Install node.js and npm
 2. Run `npm install` to install dependencies
-3. Run `npm run dev -- --host --port 3000` to start the development server
+3. Run `npm run dev -- --port 3000` to start the development server
--- fe/src/components/HomePage.vue
+++ fe/src/components/HomePage.vue
@@ -7,26 +7,50 @@
       :class="{ 'bg-slate-200': isLampOn }"
       @click="handleLampToggle"
     >
-      Lamp<span v-if="isLampOn"> - {{ lampTimer }}</span>
+      Lamp<span v-if="isLampOn"> - {{ lampTime }}</span>
     </h3>
     <h3
       class="rounded-xl border-2 border-slate-200 p-4 mb-8 select-none transition-all hover:cursor-pointer"
       :class="{ 'bg-slate-200': isACOn }"
       @click="handleACToggle"
     >
-      AC<span v-if="isACOn"> - {{ ACTimer }}</span>
+      AC<span v-if="isACOn"> - {{ acTime }}</span>
     </h3>
 
     <h2 class="mb-4">Timer (to toggle)</h2>
-    <div class="flex gap-2">
-      <h3>Lamp</h3>
-      <input type="text" class="w-16 text-right" />
-      <p>minutes</p>
-      <button
-        class="text-sm rounded-lg px-4 py-2 bg-slate-200 hover:bg-slate-300 active:bg-slate-400"
-      >
-        Set
-      </button>
+    <div class="flex gap-2 mb-4">
+      <h3 class="leading-none py-2">Lamp</h3>
+      <div class="ml-auto flex">
+        <input
+          v-model="lampTimer"
+          type="text"
+          class="w-12 text-right bg-slate-100 rounded-md px-2"
+        />
+        <p class="leading-none py-2 ml-2">minutes</p>
+        <button
+          class="ml-10 text-sm rounded-lg px-8 py-2 bg-slate-200 hover:bg-slate-300 active:bg-slate-400"
+          @click="handleLampTimer"
+        >
+          Set
+        </button>
+      </div>
+    </div>
+    <div class="flex gap-2 mb-12">
+      <h3 class="leading-none py-2">AC</h3>
+      <div class="ml-auto flex">
+        <input
+          v-model="acTimer"
+          type="text"
+          class="w-12 text-right bg-slate-100 rounded-md px-2"
+        />
+        <p class="leading-none py-2 ml-2">minutes</p>
+        <button
+          class="ml-10 text-sm rounded-lg px-8 py-2 bg-slate-200 hover:bg-slate-300 active:bg-slate-400"
+          @click="handleACTimer"
+        >
+          Set
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -38,8 +62,11 @@ export default {
       isLampOn: false,
       isACOn: false,
 
-      lampTimer: 0,
-      ACTimer: 0,
+      lampTime: 0,
+      acTime: 0,
+
+      lampTimer: 5,
+      acTimer: 10,
     };
   },
   mounted() {
@@ -59,14 +86,37 @@ export default {
 
     this.socket.addEventListener("message", (event) => {
       console.log("Received message from server", event.data);
+      this.handleStates(event.data);
     });
   },
   methods: {
     handleLampToggle() {
       this.isLampOn = !this.isLampOn;
+      const data = {
+        topic: "lamp/command",
+        message: this.isLampOn ? "ON" : "OFF",
+      };
+      this.socket.send(JSON.stringify(data));
     },
     handleACToggle() {
       this.isACOn = !this.isACOn;
+      const data = {
+        topic: "ac/command",
+        message: this.isACOn ? "ON" : "OFF",
+      };
+      this.socket.send(JSON.stringify(data));
+    },
+    handleStates(data) {
+      const { topic, message } = JSON.parse(data);
+      if (topic === "lamp/status") {
+        this.isLampOn = message === "ON";
+      } else if (topic === "ac/status") {
+        this.isACOn = message === "ON";
+      } else if (topic === "lamp/time") {
+        this.lampTime = message;
+      } else if (topic === "ac/time") {
+        this.acTime = message;
+      }
     },
   },
 };
